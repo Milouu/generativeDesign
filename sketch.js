d76a2775ca9d75a1afdcfb0acf11e8338666e2d1
Victor Timsit <victortimsit.contact@gmail.com>
2020-02-03 16:35:55 +0100

    :alembic: Rotate

--- sketch.js
+++ sketch.js
@@ -2,7 +2,8 @@
 let param = {
   width: 10,
   height: 10,
-  count: 400,
+  count: 800,
+  shapeCount: 2,
 }
 
 // Set global
@@ -20,17 +21,19 @@ function setup() {
   param.paramColor = color(237, 0, 62)
   param.bgColor = color(253, 253, 253)
   param.randomColors = []
+  param.colors = []
 
   for(let i = 0; i < 10; i++) {
     let x = random(webImage.width)
     let y = random(webImage.height)
     let c = webImage.get(x , y)
     param.randomColors.push(c)
-    console.log('randomColor')
   }
 
   // Set random color once
-  param.color = param.randomColors[Math.floor(random(param.randomColors.length))]
+  for(let i = 0; i < param.shapeCount; i++) {
+    param.colors.push(param.randomColors[Math.floor(random(param.randomColors.length))])
+  }
 
   seed = random(500)
 
@@ -40,17 +43,22 @@ function setup() {
 
 function draw() {
   background(param.bgColor)
-  noStroke()
+  strokeWeight(4)
   randomSeed(seed)
 
-  for(let i = 0; i < param.count; i++) {
-    
-    let x = random(window.innerWidth)
-    let y = random(window.innerHeight)
-    fill(param.color)
-    rect(x, y, param.width, param.height)
+  for(let j = 0; j < param.shapeCount; j++) {
+    stroke(param.colors[j])
+    for(let i = 0; i < param.count; i++) {
+      let x = random(window.innerWidth)
+      let y = random(window.innerHeight)
+
+      rotate(PI / random(3))
+
+      if(j == 0) rect(x, y, param.width, param.height)
+      if(j == 1) triangle(x + 60, y + 60, x + 60, y + 20, x + 20, y + 20)
+    }
   }
-}
+  }  
 
 function keyTyped() {
   if(key === 's') {
