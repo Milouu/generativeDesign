0e1b573092b7346637422ce358202e8af42ab0bf
Milouu <milan.roustan@gmail.com>
2020-02-03 16:36:46 +0100

    :construction: Test on pattern & colors by ingredient list

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 // Set params
 let param = {
-  width: 10,
-  height: 10,
+  width: 100,
+  height: 100,
   count: 400,
 }
 
@@ -9,28 +9,38 @@ let param = {
 let seed
 
 function preload() {
-  webImage = loadImage('https://images.unsplash.com/photo-1580170525195-16a47393afab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=931&q=80')
+  webImage = loadImage('./assets/images/all_berries.jpg')
 }
 
 function setup() {
   // Init canvas
   createCanvas(window.innerWidth, window.innerHeight)
 
+  param.ingredients = ['lorem', 'ipsum', 'dolor', 'test', 'sah']
+  param.patterns = []
+
   // Set color
   param.paramColor = color(237, 0, 62)
   param.bgColor = color(253, 253, 253)
   param.randomColors = []
+
+  
 
   for(let i = 0; i < 10; i++) {
     let x = random(webImage.width)
     let y = random(webImage.height)
     let c = webImage.get(x , y)
     param.randomColors.push(c)
-    console.log('randomColor')
   }
 
   // Set random color once
-  param.color = param.randomColors[Math.floor(random(param.randomColors.length))]
+  // param.color = param.randomColors[Math.floor(random(param.randomColors.length))]
+
+  for(let i = 0; i < param.ingredients.length; i++) {
+    const pattern = {}
+    pattern.color = param.randomColors[Math.floor(random(param.randomColors.length))]
+    param.patterns[i] = pattern
+  }
 
   seed = random(500)
 
@@ -40,15 +50,26 @@ function setup() {
 
 function draw() {
   background(param.bgColor)
-  noStroke()
+  // noStroke()
+  strokeWeight(4)
   randomSeed(seed)
 
   for(let i = 0; i < param.count; i++) {
-    
-    let x = random(window.innerWidth)
-    let y = random(window.innerHeight)
-    fill(param.color)
-    rect(x, y, param.width, param.height)
+    for(let j = 0; j < param.ingredients.length; j++) {
+      let x = random(window.innerWidth)
+      let y = random(window.innerHeight)
+      // fill(param.patterns[j].color)
+      stroke(param.patterns[j].color)
+      
+      rotate(PI / random(3))
+      // rect(x, y, param.width, param.height)
+      let width = random(param.width)
+      beginShape(TRIANGLES)
+      vertex(x, y)
+      vertex(x + width/2, y + random(param.height))
+      vertex(x + width, y)
+      endShape()
+    }
   }
 }
 
