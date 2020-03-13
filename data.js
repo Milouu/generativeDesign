93f11b47ca46e7fcaf92711c657749cf746b383b
Milouu <milan.roustan@gmail.com>
2020-03-13 15:04:14 +0100

    Data and images for fruit & corn cereals

--- data.js
+++ data.js
@@ -24,3 +24,58 @@ const chocolate = {
     }
   ]
 }
+
+const fruit = {
+  "name": "Fruit Cereals",
+  "slug": "fruit_cereals",
+  "ingredients": [
+    {
+      "name": "Farine de blé complet",
+      "percentage": 33,
+    },
+    {
+      "name": "Fruits",
+      "percentage": 26,
+    },
+    {
+      "name": "Farine de Maïs",
+      "percentage": 22,
+    },
+    {
+      "name": "Sucre de canne non raffiné",
+      "percentage": 15,
+    },
+    {
+      "name": "Vitamines et minéraux",
+      "percentage": 4,
+    },
+    
+  ]
+}
+
+const corn = {
+  "name": "Corn Cereals",
+  "slug": "corn_cereals",
+  "ingredients": [
+    {
+      "name": "Maïs",
+      "percentage": 57,
+    },
+    {
+      "name": "Sucre de canne non raffiné",
+      "percentage": 17,
+    },
+    {
+      "name": "Arôme de malt",
+      "percentage": 12,
+    },
+    {
+      "name": "Arôme d'orge",
+      "percentage": 10,
+    },
+    {
+      "name": "Vitamines et minéraux",
+      "percentage": 4,
+    }, 
+  ]
+}
